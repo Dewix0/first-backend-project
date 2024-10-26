dec3b1b4326cd69d5cf94d9fff41d8a9dacbe4a2
Даниил Батуров <danbat2006@gmail.com>
2024-10-26 14:44:35 +0300

    Добавление всех файлов проекта

---
+++ router.py
@@ -0,0 +1,29 @@
+from fastapi import  Depends, APIRouter
+
+from typing import Annotated
+
+from repository import TasksRepository
+
+from shemes import STaskAdd, STask , STaskId
+
+
+router=APIRouter(
+    prefix="/tasks",
+    tags=["Таски"],
+)
+
+@router.post("/tasks")
+async def add_task(
+    task: Annotated[STaskAdd,Depends()],
+    
+    
+)-> STaskId:
+    task_id=await TasksRepository.add_one(task)
+    return {"ok":True, "task_id":task_id}
+
+
+
+@router.get("/tasks")
+async def get_tasks() -> list[STask]:
+    tasks= await TasksRepository.find_all()
+    return {"data":tasks}
